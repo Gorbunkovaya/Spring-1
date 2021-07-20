b897cd99df610c327de859ac625300e5e80e9aea
Gorbunkovaya <dgorbunkovaya@gmail.com>
2021-07-20 22:24:27 +0300

    upd

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat newCat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(newCat1.getName());
+        Cat newCat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(newCat2.getName());
+
+        boolean compareHW = bean == bean1;
+        boolean compareCat = newCat1 == newCat2;
+        System.out.println(compareHW);
+        System.out.println(compareCat);
     }
 }
